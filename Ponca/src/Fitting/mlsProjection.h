86c4530a12869f8a9947605331d65013fb526d4d
Thibault Lejemble <thibault.le@live.fr>
2023-02-11 00:25:10 +0100

    projection

---
+++ Ponca/src/Fitting/mlsProjection.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <Ponca/Fitting>
+
+namespace Ponca {
+
+/*!
+    \brief Quasi-Orthogonal Moving Least Squares projection
+    
+    See Section 4.2 of
+    On Normals and Projection Operators for Surfaces Defined by Point Sets
+    Marc Alexa and Anders Adamson
+    Eurographics Symposium on Point-Based Graphics (2004)
+
+    \note Also called 'almost' orthogonal projection
+    \tparam FitT is the main Concept::FittingProcedureConcept used during the first N-1 steps
+    \tparam FitFinalT is another Concept::FittingProcedureConcept used for the N-th fitting step (default is FitT)
+ */
+template<class FitT, class FitFinalT = FitT>
+class QuasiOrthogonalMLSProjection
+{
+public:
+    using Scalar        = typename FitT::Scalar;
+    using VectorType    = typename FitT::VectorType;
+    using WFunctor      = typename FitT::WFunctor;
+    using WFunctorFinal = typename FitFinalT::WFunctor;
+
+public:
+    /*!
+        \brief Perform the fitting to nei and return the projection of p
+        \tparam NeighborQueryT must provide an operator()(const VectorType& q, Scalar t)
+        that returns a range over the neighbors of q at a distance t (of type FitT::DataPoint)
+    */
+    template<class NeighborQueryT>
+    VectorType project(const VectorType& p, const NeighborQueryT& nei);
+
+public:
+    void setScale(Scalar scale) {m_scale = scale;}
+    void setStepMax(int step) {m_stepMax = step;}
+    void setConvRatio(Scalar convRatio) {m_convRatio = convRatio;}
+
+protected:
+    int    m_stepMax{20};       /*!< \brief Maximal number of iterations */
+    Scalar m_convRatio{0.001};  /*!< \brief Distance threshold for convergence (ratio of scale) */
+    Scalar m_scale{0.0};        /*!< \brief Weighting kernel support size */
+
+public:
+    FitFinalT m_fitFinal;       /*!< \brief The final fit can be used after a call to project */
+    int m_step{0};
+    bool m_converged{false};
+};
+
+} // namespace Ponca
+
+#include "mlsProjection.hpp"
---
+++ Ponca/src/Fitting/mlsProjection.hpp
@@ -0,0 +1,69 @@
+#include "mlsProjection.h"
+
+namespace Ponca {
+
+template<class FitT, class FitFinalT>
+template<class NeighborQueryT>
+typename QuasiOrthogonalMLSProjection<FitT,FitFinalT>::VectorType
+QuasiOrthogonalMLSProjection<FitT,FitFinalT>::project(const VectorType& p, const NeighborQueryT& nei)
+{
+    auto q = p; // moving point
+
+    FitT fit;
+    m_fitFinal = FitFinalT();
+
+    fit.setWeightFunc(WFunctor(m_scale));
+    m_fitFinal.setWeightFunc(WFunctorFinal(m_scale));
+
+    const auto epsilon = m_convRatio * m_scale;
+    const auto epsilon2 = epsilon * epsilon;
+
+    // Three stopping conditions:
+    //   1. reach convergence (displacement is below epsilon)
+    //   2. reach step max - 1 (save one final step)
+    //   3. fitting is not stable (see return statement below)
+    m_converged = false;
+    for(m_step = 0; m_step < m_stepMax-1 and not m_converged; ++m_step)
+    {
+        fit.init(q);
+
+        auto status = Ponca::FIT_RESULT::NEED_OTHER_PASS;
+        while(status == Ponca::FIT_RESULT::NEED_OTHER_PASS)
+        {
+            for(const auto& n : nei(q, m_scale))
+            {
+                fit.addNeighbor(n);
+            }
+            status = fit.finalize();
+        }
+
+        if(status != Ponca::FIT_RESULT::STABLE)
+        {
+            return q;
+        }
+
+        const VectorType proj = fit.project(p); // project the initial point
+        const Scalar dist2 = (proj - q).squaredNorm();
+        m_converged = dist2 < epsilon2;
+        q = proj;
+    }
+
+    // final step
+    {
+        ++m_step;
+        m_fitFinal.init(q);
+
+        auto status = Ponca::FIT_RESULT::NEED_OTHER_PASS;
+        while(status == Ponca::FIT_RESULT::NEED_OTHER_PASS)
+        {
+            for(const auto& n : nei(q, m_scale))
+            {
+                m_fitFinal.addNeighbor(n);
+            }
+            status = m_fitFinal.finalize();
+        }
+        return status == Ponca::FIT_RESULT::STABLE ? m_fitFinal.project(p) : q;
+    }
+}
+
+} // namespace Ponca
